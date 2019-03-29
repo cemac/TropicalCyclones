98ea2fc4d508543381a15bfccbec588fcb95bde3
hlburns <H.L.Burns@leeds.ac.uk>
2019-03-29 12:51:27 +0000

    :rotating_light::wrench: Checking modules required

---
+++ TEST/windspeed.py
@@ -0,0 +1,10 @@
+import numpy as np
+import iris
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import matplotlib
+from matplotlib.colors import from_levels_and_colors
+import matplotlib.ticker as mticker
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+import matplotlib.cm as mpl_cm
