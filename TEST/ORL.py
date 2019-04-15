1092759f3fa853d7dceb3ac91df5fbff7c7f2b82
hlburns <H.L.Burns@leeds.ac.uk>
2019-04-15 13:51:20 +0100

    :hammer_and_wrench: skeleton version

--- TEST/ORL.py
+++ TEST/ORL.py
@@ -31,27 +31,13 @@ import ast
 from toolkit import *
 
 
-def calculate_pot_temp(pressure, temperature):
-        return temperature * (1000 / pressure)**(0.286)
-
-def calc_grad(data, dx):
-    # Basic central differencing
-    # Data should be 1-d
-    grad = np.zeros(data.shape)
-    grad[0] = (1 / dx) * (data[1] - data[0])
-    grad[-1] = (1 / dx) * (data[-1] - data[-2])
-    grad[1:-1] = (1 / (2 * dx)) * (data[2:] - data[:-2])
-    return grad
-
-
 class ORL(object):
     '''ORL
-        
-
     Members:
 
     '''
-    def __init__(s, configfile='configfile', stashfile='stashvars',
+
+    def __init__(self, configfile='configfile', stashfile='stashvars',
                  plotfile='plotconf'):
         '''
         Args:
@@ -61,69 +47,69 @@ class ORL(object):
         # Read configuration file to import settings
         conf_df = pd.read_csv('configfile.csv')
         # Define all constraints and locate data.
-        s.data_loc = conf_df.data_loc[0]
-        s.data_name = conf_df.data_name[0]
-        s.outfile_loc = conf_df.outfile_loc[0]
-        s.outfile_name = conf_df.outfile_name .values
-        s.ens_members = np.arange(int(conf_df.ens_members[0]))
-        s.plev = int(conf_df.plev[0])
-        s.md = int(conf_df.md[0])
-        s.TT = int(conf_df.TT[0])
-        s.model = conf_df.model[0]
-        s.v_times = ast.literal_eval(conf_df.v_times[0])
-        s.init_day = int(conf_df.init_day[0])
-        s.init_time = int(conf_df.init_time[0])
-        s.final_day = int(conf_df.final_day[0])
-        s.final_time = int(conf_df.final_time[0])
-        s.v_days = np.arange(s.init_day, s.init_day + 6, 1)
-        s.yr = int(conf_df.yr[0])
-        s.mth = int(conf_df.mth[0])
-        s.domain_rad = float(conf_df.domain_rad[0])
+        self.data_loc = conf_df.data_loc[0]
+        self.data_name = conf_df.data_name[0]
+        self.outfile_loc = conf_df.outfile_loc[0]
+        self.outfile_name = conf_df.outfile_name .values
+        self.ens_members = np.arange(int(conf_df.ens_members[0]))
+        self.plev = int(conf_df.plev[0])
+        self.md = int(conf_df.md[0])
+        self.TT = int(conf_df.TT[0])
+        self.model = conf_df.model[0]
+        self.v_times = ast.literal_eval(conf_df.v_times[0])
+        self.init_day = int(conf_df.init_day[0])
+        self.init_time = int(conf_df.init_time[0])
+        self.final_day = int(conf_df.final_day[0])
+        self.final_time = int(conf_df.final_time[0])
+        self.v_days = np.arange(self.init_day, self.init_day + 6, 1)
+        self.yr = int(conf_df.yr[0])
+        self.mth = int(conf_df.mth[0])
+        self.domain_rad = float(conf_df.domain_rad[0])
         print('Loaded configuration settings')
         # add stash codes
         stash_df = pd.read_csv('stashvars.csv')
         u_stash = stash_df.u_stash[0]
         v_stash = stash_df.v_stash[0]
-        s.slp_stash = stash_df.slp_stash[0]
-        s.u_constraint = iris.AttributeConstraint(STASH=u_stash)
-        s.v_constraint = iris.AttributeConstraint(STASH=v_stash)
-        s.froot = conf_df.track_data_root[0]
-        s.fpat = conf_df.track_data_metadata[0]
+        self.slp_stash = stash_df.slp_stash[0]
+        self.u_constraint = iris.AttributeConstraint(STASH=u_stash)
+        self.v_constraint = iris.AttributeConstraint(STASH=v_stash)
+        self.froot = conf_df.track_data_root[0]
+        self.fpat = conf_df.track_data_metadata[0]
         # Determine the dimensions of the stamp plot according to no members
-        s.n_ems = len(s.ens_members)
-        s.nrows, s.ncols = find_subplot_dims(s.n_ems)
-        s.p_constraint = iris.Constraint(pressure=s.plev)
+        self.n_ems = len(self.ens_members)
+        self.nrows, self.ncols = find_subplot_dims(self.n_ems)
+        self.p_constraint = iris.Constraint(pressure=self.plev)
         print('Loaded stash codes')
 
         # Plotting configuration
-        s.plot_df = pd.read_csv('plot_conf.csv')
+        self.plot_df = pd.read_csv('plot_conf.csv')
         print('Loaded plot settings file')
 
-    def loop(s):
-        for dd in s.vdays:
-            for hr in s.vtimes:
-                s.dayhour(s, s.yr, s.mth, dd, hr, s.init_day, s.final_day,
-                          s.init_time, s.final_time)
+    def loop(self):
+        for dd in self.vdays:
+            for hr in self.vtimes:
+                self.dayhour(self.yr, self.mth, dd, hr, self.init_day,
+                             self.final_day, self.init_time, self.final_time)
 
-    def dayhour(s, yr, mm, dd, hr, d0, dN, t0, tN):
-        outfile, tcon, bcon, ltime = s.t_const(yr, mm, dd, hr, d0,
-                                               dN, t0, tN)
+    def dayhour(self, yr, mm, dd, hr, d0, dN, t0, tN):
+        outfile, tcon, bcon, ltime = self.t_const(yr, mm, dd, hr, d0,
+                                                  dN, t0, tN)
         if ltime is False:
             return
         # Create figure
-        fig, axs = plt.subplots(s.nrows, s.ncols, dpi=100, subplot_kw={
+        fig, axs = plt.subplots(self.nrows, self.ncols, dpi=100, subplot_kw={
                                 'projection': ccrs.PlateCarree()})
-        for i, em in enumerate(s.ens_members):
+        for i, em in enumerate(self.ens_members):
             # Determine figure coordinate
-            ic = i % s.ncols
-            ir = i / s.ncols
+            ic = i % self.ncols
+            ir = i / self.ncols
             if len(axs.shape) > 1:
                 ax = axs[ir, ic]
             else:
                 ax = axs[ic]
-            llab, blab = label_fixer(i, s.ncols, s.nrows)
-            wspeed_plt = s.plot_i(ax, dd, hr, em, outfile, tcon, bcon, ltime,
-                                  llab, blab)
+            llab, blab = label_fixer(i, self.ncols, self.nrows)
+            wspeed_plt = self.plot_i(ax, dd, hr, em, outfile, tcon, bcon,
+                                     ltime, llab, blab)
         # Reduce white space and then make whole figure bigger,
         # keeping the aspect ratio constant.
         plt.gcf().subplots_adjust(hspace=0.025, wspace=0.025, bottom=0.05,
@@ -139,11 +125,11 @@ class ORL(object):
         cbar.set_label('ms$^{-1}$', size=18)
         axs = fig.gca()
         string1 = ('Initial time: {0}/{1}/{2}, {3:02d}' +
-                   'Z').format(str(s.md)[-2:], str(s.md)[:2],
-                               s.yr, s.TT)
+                   'Z').format(str(self.md)[-2:], str(self.md)[:2],
+                               self.yr, self.TT)
         xy1 = [0.95, 0.95]
         string2 = ('Valid time: {0:02d}/{1:02d}/{2}, {3:02d}' +
-                   'Z').format(dd, s.mth, s.yr, hr)
+                   'Z').format(dd, self.mth, self.yr, hr)
         xy2 = [0.95, 0.925]
         string3 = 'T+{0}Z'.format(ltime)
         xy3 = [0.95, 0.9]
@@ -155,12 +141,12 @@ class ORL(object):
 
     def plot_i(s, ax, dd, hr, em, outfile, tcon, bcon, ltime, llab, blab):
         # Load the data for this ensemble member at this time
-        df = s.data_loc + s.data_name + '{0:02d}.pp'.format(em)
-        u = uv(df, s.u_constraint, tcon, s.p_constraint, bcon)
-        v = uv(df, s.v_constraint, tcon, s.p_constraint, bcon)
+        df = self.data_loc + self.data_name + '{0:02d}.pp'.format(em)
+        u = uv(df, self.u_constraint, tcon, self.p_constraint, bcon)
+        v = uv(df, self.v_constraint, tcon, self.p_constraint, bcon)
         u, v, ws = winds(u, v, em)
         wspeed_plt = plot_wspeed(ax, ws)
-        plot_winds(ax, u, v, s.model)
+        plot_winds(ax, u, v, self.model)
         # Add grid lines and ticks etc.
         map_formatter(ax, bottom_label=blab, left_label=llab, labelsize=20,
                       tick_base_x=2, tick_base_y=2)
@@ -180,11 +166,11 @@ class ORL(object):
         tcon = iris.Constraint(time=iris.time.PartialDateTime(year=yr,
                                                               month=mm, day=dd,
                                                               hour=hr))
-        out = s.outfile_loc + s.outfile_name
+        out = self.outfile_loc + self.outfile_name
         outfile = out + '{0:02d}_{1:02d}Z.png'.format(dd, hr)
         # Fix domain according to position of ensemble member 0,
         # this assumes the position is similar in ensemble members
-        mmll = find_centre_3h(s.md, s.TT, 0, dd, hr, s.model,
-                              s.froot, s.fpat, s.domain_rad)
+        mmll = find_centre_3h(self.md, self.TT, 0, dd, hr, self.model,
+                              self.froot, self.fpat, self.domain_rad)
         bcon = box_constraint(mmll[0], mmll[1], mmll[2], mmll[3])
         return outfile[0], tcon, bcon, ltime
